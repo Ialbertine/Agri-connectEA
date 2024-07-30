e1194b921ad216fccfc03c733b2d2d39c407669e
Ialbertine <a.ingabire1@alustudent.com>
2024-07-30 21:54:36 +0200

    landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { PiArrowBendDownLeftThin } from "react-icons/pi";
 import { FaTwitter } from "react-icons/fa6";
@@ -6,14 +6,16 @@ import { MdOutlineFacebook } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa6";
 import { FaPinterestP } from "react-icons/fa6";
 
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Home = () => {
   useEffect(() => {
     AOS.init({
-      duration: 2000,});
-  },[]);
+      duration: 2000,
+    });
+  }, []);
 
   return (
     <div>
@@ -22,7 +24,10 @@ const Home = () => {
           src="img/photo3.jpg"
           className=" lg:h-[100vh] md:h-[90vh] sm:h-[100vh] w-full object-cover"
         ></img>
-        <div className="text-white mt-[7rem] absolute lg:top-17 md:top-14 sm:top-10 lg:left-48 md:left-20 sm:left-10 flex flex-col lg:gap-7 md:gap-7 sm:gap-5" data-aos="fade-up">
+        <div
+          className="text-white mt-[7rem] absolute lg:top-17 md:top-14 sm:top-10 lg:left-48 md:left-20 sm:left-10 flex flex-col lg:gap-7 md:gap-7 sm:gap-5"
+          data-aos="fade-up"
+        >
           <p className="underline underline-offset-8">
             WE'RE AGRISUPPLY CHAIN COMMUNITY
           </p>
@@ -49,10 +54,19 @@ const Home = () => {
       </div>
       <div className=" relative">
         <div className="absolute bottom-0 left-60"></div>
-        <div className=" w-[6rem] h-[8rem] bg-[#0b5727] opacity-60 absolute rounded-r-xl top-32 left-0" data-aos="fade-right"></div>
-        <div className="w-[5rem] h-[5rem] bg-yellow-400 opacity-90 absolute  rounded-2xl top-[36vh] left-12" data-aos="fade-left"></div>
+        <div
+          className=" w-[6rem] h-[8rem] bg-[#0b5727] opacity-60 absolute rounded-r-xl top-32 left-0"
+          data-aos="fade-right"
+        ></div>
+        <div
+          className="w-[5rem] h-[5rem] bg-yellow-400 opacity-90 absolute  rounded-2xl top-[36vh] left-12"
+          data-aos="fade-left"
+        ></div>
         <div className="py-32 flex lg:flex-row md:flex-row sm:flex-col items-center justify-center gap-10">
-          <div className="lg:w-[69vh] md:w-[44vh] sm:w-[44vh] flex flex-col lg:p-0 md:p-0 sm:p-3 gap-3" data-aos="fade-right">
+          <div
+            className="lg:w-[69vh] md:w-[44vh] sm:w-[44vh] flex flex-col lg:p-0 md:p-0 sm:p-3 gap-3"
+            data-aos="fade-right"
+          >
             <p className="text-lg">Get to know our priority:</p>
             <p className="text-4xl text-green-900">Harvest Showcasing</p>
             <p className="text-lg text-justify sm:w-[60vh]">
@@ -78,65 +92,115 @@ const Home = () => {
         </div>
       </div>
 
-    {/* Missions */}
+      {/* Missions */}
 
-    <div className="bg-green-700 text-white h-100 flex lg:flex-row md:flex-col sm:flex-col items-center justify-evenly px-40 py-20 lg:pl-32 md:pl-0 sm:pl-10">
-          <div className='flex flex-col gap-7 md:pl-0 lg:pl-0 sm:pl-32 mb-10'>
-            <p className='text-2xl font-bold'>MISSION</p>
-            <div>
-              <p className=' lg:border-r-2 border-gray-400 lg:w-[60vh] md:w-[60vh] sm:w-[70vh] text-lg pr-5'>create a transparent, efficient, and inclusive agricultural marketplace that connects farmers, buyers,
-                and other stakeholders, enabling fair and profitable transactions while promoting sustainability
-                and economic growth in rural communities.
-              </p>
-            </div>
-
-          </div>
-          <div className='flex flex-col gap-7 sm:pl-16 mb-10'>
-            <p className='text-2xl md:pl-0 sm:pl-7 font-bold'>VISION</p>
-            <p className='lg:border-r-2 border-gray-400 lg:w-[60vh] md:w-[70vh] sm:w-[70vh] lg:pl-0 md:pl-0 sm:pl-8 text-lg pr-5'>
-              revolutionize agricultural trade by leveraging technology to bridge the gap
-              between producers and consumers, ensuring food security, empowering farmers, and fostering prosperity
-              across the agricultural value chain.
+      <div
+        className="text-white h-[450px] flex lg:flex-row md:flex-col sm:flex-col items-center 
+      justify-evenly px-40 py-20 lg:pl-32 md:pl-0 sm:pl-10 bg-cover bg-center"
+        style={{ backgroundImage: "url(/img/harvest5.jpg)" }}
+      >
+        <div className="flex flex-col gap-7 md:pl-0 lg:pl-0 sm:pl-32 mb-10">
+          <p className="text-2xl font-bold">MISSION</p>
+          <div>
+            <p className=" lg:border-r-2 border-gray-400 lg:w-[60vh] md:w-[60vh] sm:w-[70vh] text-lg pr-5">
+              Our mission is to empower farmers and agricultural cooperatives in
+              East Africa by enhancing their market access, connects them with
+              buyers, and facilitates information exchange. We aims to foster
+              sustainable agricultural practices, increase farmer incomes, and
+              promote both regional trade of agricultural products.
             </p>
           </div>
         </div>
-       {/* testimonials */}
+        <div className="flex flex-col gap-7 sm:pl-16 mb-10">
+          <p className="text-2xl md:pl-0 sm:pl-7 font-bold">VISION</p>
+          <p className="lg:border-r-2 border-gray-400 lg:w-[60vh] md:w-[70vh] sm:w-[70vh] lg:pl-0 md:pl-0 sm:pl-8 text-lg pr-5">
+            Our goal is to establish a robust and dynamic agricultural market
+            that drives economic growth and ensures food security in the area.
+            By bridging the gap between producers and buyers, we aim to create
+            an environment where farmers can thrive and contribute to the
+            overall prosperity of the region.
+          </p>
+        </div>
+      </div>
+      {/* testimonials */}
 
-       {/* Get in touch with us */}
-       <div>
-          <div className='flex lg:flex-row md:flex-col sm:flex-col justify-evenly py-28  bg-white'>
-            <div className='lg:w-[44vh] md:w-[80vh] mb-10 sm:w-[44vh] flex flex-col lg:p-0 md:p-0 sm:p-3 gap-3  lg:ml-0 md:ml-20 sm:ml-7'>
-              <p className=''>Contact now</p>
-              <p className='text-5xl text-green-900 lg:w-[50vh]' data-aos="fade-right">Get in touch with us</p>
-              <p className=''>our contact information reflects our commitment to providing excellent
-                customer service and support to all users of our Agri-connectEA platform. Feel free to
-                reach out to us with any questions or needs you may have.
-              </p>
-              <div className='flex gap-3' data-aos="fade-up">
-                <div className='rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl' ><FaTwitter /></div>
-                <div className='rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl' ><MdOutlineFacebook /></div>
-                <div className='rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl' ><FaPinterestP /></div>
-                <div className='rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl' ><FaInstagram /></div>
+      
+      {/* Get in touch with us */}
+      <div>
+        <div className="flex lg:flex-row md:flex-col sm:flex-col justify-evenly py-28  bg-white">
+          <div className="lg:w-[44vh] md:w-[80vh] mb-10 sm:w-[44vh] flex flex-col lg:p-0 md:p-0 sm:p-3 gap-3  lg:ml-0 md:ml-20 sm:ml-7">
+            <p className="">Contact now</p>
+            <p
+              className="text-5xl text-green-900 lg:w-[50vh]"
+              data-aos="fade-right"
+            >
+              Get in touch with us
+            </p>
+            <p className="">
+              our contact information reflects our commitment to providing
+              excellent customer service and support to all users of our
+              Agri-connectEA platform. Feel free to reach out to us with any
+              questions or needs you may have.
+            </p>
+            <div className="flex gap-3" data-aos="fade-up">
+              <div className="rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl">
+                <FaTwitter />
               </div>
-            </div>
-            <div className='flex flex-col gap-5 lg:ml-0 md:ml-20 sm:ml-7' data-aos="fade-up">
-              <div className='flex lg:flex-row md:flex-col sm:flex-col gap-5'>
-                <input type='text' placeholder='Names' className='bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]'></input>
-                <input type='email' placeholder='Email' className='bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]'></input>
+              <div className="rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl">
+                <MdOutlineFacebook />
               </div>
-              <div className='flex lg:flex-row md:flex-col sm:flex-col gap-5 '>
-                <input type='phone' placeholder='Phone number' className='bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]'></input>
-                <input type='text' placeholder='Subject' className='bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]'></input>
+              <div className="rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl">
+                <FaPinterestP />
               </div>
-              <div>
-                <textarea placeholder='Write message' className='bg-[#ebebea] rounded-xl p-2 lg:w-[83vh] md:w-[80vh] sm:w-[40vh] h-[30vh]'></textarea>
+              <div className="rounded-full bg-[#cfcfcb] flex items-center justify-center w-11 h-11 opacity-60 text-2xl">
+                <FaInstagram />
               </div>
-              <Link to='/contact' className=' bg-yellow-200 px-5 py-3 lg:w-[30vh] md:w-[27vh] sm:w-[27vh] text-xl rounded-xl text-black hover:text-white hover:bg-green-900'> Send message</Link>
-
             </div>
           </div>
-
+          <div
+            className="flex flex-col gap-5 lg:ml-0 md:ml-20 sm:ml-7"
+            data-aos="fade-up"
+          >
+            <div className="flex lg:flex-row md:flex-col sm:flex-col gap-5">
+              <input
+                type="text"
+                placeholder="Names"
+                className="bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]"
+              ></input>
+              <input
+                type="email"
+                placeholder="Email"
+                className="bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]"
+              ></input>
+            </div>
+            <div className="flex lg:flex-row md:flex-col sm:flex-col gap-5 ">
+              <input
+                type="phone"
+                placeholder="Phone number"
+                className="bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]"
+              ></input>
+              <input
+                type="text"
+                placeholder="Subject"
+                className="bg-[#ebebea] rounded-xl p-5 lg:w-[40vh] md:w-[80vh] sm:w-[40vh]"
+              ></input>
+            </div>
+            <div>
+              <textarea
+                placeholder="Write message"
+                className="bg-[#ebebea] rounded-xl p-2 lg:w-[83vh] md:w-[80vh] sm:w-[40vh] h-[30vh]"
+              ></textarea>
+            </div>
+            <Link
+              to="/contact"
+              className=" bg-yellow-200 px-5 py-3 lg:w-[30vh] md:w-[27vh] sm:w-[27vh] text-xl rounded-xl text-black hover:text-white hover:bg-green-900"
+            >
+              {" "}
+              Send message
+            </Link>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
